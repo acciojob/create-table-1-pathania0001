be572f3da1b0f84d2986e56236fca9c777ae7e80
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-15 13:32:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+
+const table = document.getElementById("sampleTable");
 function insert_Row() {
-    //Write your code here
-  
-  
+    const newRow = document.createElement("tr");
+	
+	const leftCol = document.createElement("td");
+	leftCol.innerText="New Cell1";
+	const reightCol = document.createElement("td");
+	reightCol.innerText="New Cell2";
+	
+	newRow.appendChild(leftCol);
+	newRow.appendChild(reightCol);
+	table.appendChild(newRow);
+	return;
 }
